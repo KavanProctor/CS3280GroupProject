0e61414aa2de8642cfcf14ff099c50fdd641bd76
Michael Rasmussen <michaelrasmussen5@mail.weber.edu>
2025-07-23 22:15:45 -0600

    outlined logic

---
+++ Main/clsMainLogic.cs
@@ -0,0 +1,13 @@
+namespace CS3280GroupProject.Main
+{
+    // struct Invoice;
+    
+    public class clsMainLogic
+    {
+        // public void CreateInvoice(string date, List<string> items);
+        // public void InvoiceSetItems(int invoiceID, string item);
+        // public void InvoiceSetDate(int invoiceID, string date);
+        // public Invoice GetInvoice(int invoiceID);
+        // public Invoice GetLatestInvoice();
+    }
+}
