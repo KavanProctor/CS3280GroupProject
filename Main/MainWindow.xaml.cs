7e9db155eafd59c29d5f7d2a776fcaf278b0e131
Michael Rasmussen <michaelrasmussen5@mail.weber.edu>
2025-08-06 10:18:55 -0600

    implemented the basic elements

--- Main/MainWindow.xaml.cs
+++ Main/MainWindow.xaml.cs
@@ -23,19 +23,70 @@ namespace CS3280GroupProject.Main
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<clsItem> items;
+
+        private decimal cost = 0;
+        private List<clsItem> itemsBuffer = new();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.addItemButton.Click += delegate{
+                if(this.itemSelection.SelectedIndex < 0) return;
+
+                clsItem item = this.items[this.itemSelection.SelectedIndex];
+                if(!this.itemsBuffer.Any(i => i.ItemCode == item.ItemCode))
+                {
+                    this.itemsBuffer.Add(item);
+
+                    this.SyncUI();
+                }
+            };
+
+            this.Reset();
+        }
+
+        private void Reset(){
+            this.items = clsMainLogic.GetItems();
+            this.itemSelection.Items.Clear();
+            this.items.ForEach(i => this.itemSelection.Items.Add(i.ItemCode));
+
+            this.itemsBuffer.Clear();
+
+            this.invoiceID.Text = "TBD";
+
+            this.invoiceItems.Items.Clear();
+
+            this.SyncUI();
+        }
+
+        private void SyncUI(){
+            this.cost = 0;
+            foreach(clsItem item in this.itemsBuffer)
+            {
+                this.cost += item.Cost;
+            }
+
+            // TODO: update this.invoiceItems
+
+            this.invoiceCost.Text = this.cost.ToString();
         }
 
         private void UpdateItems(object sender, EventArgs ev)
         {
+            // NOTE: the requirements say that the items window cannot be opened if an invoice is being created/edited (which thankfully makes everything much simpler)
+            // TODO: either prevent the items window from opening (when appropriate) or confirm that the user is okay with losing unsaved info
+
             ItemWindow itemWnd = new ItemWindow();
             this.Hide();
             itemWnd.ShowDialog();
             this.Show();
 
             // TODO: check this.itemWnd.bHasItemBeenChanged and if its true then update the items combo box by updating its ItemsSource according to clsMainLogic.GetInvoice().Items
+            // NOTE: this todo can probably be ignored? just always call this.Reset ...
+
+            this.Reset();
         }
         private void SelectInvoice(object sender, EventArgs ev)
         {
--- Main/clsMainLogic.cs
+++ Main/clsMainLogic.cs
@@ -1,13 +1,50 @@
+using System.Data;
+using System.IO;
+using System.Data.OleDb;
+
+using CS3280GroupProject.Items;
+
 namespace CS3280GroupProject.Main
 {
     // struct Invoice;
     
     public class clsMainLogic
     {
+        private static DataSet ExecSQL(string sql)
+        {
+            DataSet result = new DataSet();
+
+            using (OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data source= " + Directory.GetCurrentDirectory() + "\\Invoice.mdb"))
+            {
+                using (OleDbDataAdapter adapter = new OleDbDataAdapter())
+                {
+                    conn.Open();
+
+                    adapter.SelectCommand = new OleDbCommand(sql, conn);
+                    adapter.SelectCommand.CommandTimeout = 0;
+
+                    adapter.Fill(result);
+                }
+            }
+
+            return result;
+        }
+
         // public void CreateInvoice(string date, List<string> items);
         // public void InvoiceSetItems(int invoiceID, string item);
         // public void InvoiceSetDate(int invoiceID, string date);
         // public Invoice GetInvoice(int invoiceID);
         // public Invoice GetLatestInvoice();
+
+        public static List<clsItem> GetItems(){
+            return ExecSQL(clsMainSQL.GetItems()).Tables[0].Rows.OfType<DataRow>().Select(r => {
+                return new clsItem()
+                {
+                    ItemCode=(string)r[0],
+                    ItemDesc=(string)r[1],
+                    Cost=(decimal)r[2],
+                };
+            }).ToList();
+        }
     }
 }
