4b5069c8fd374d280882ec313413913cb577af7f
Jake Runyan <jake.runyan5@gmail.com>
2025-08-06 22:20:51 -0600

    Finished UI and working on filters right now

--- Search/SearchWindow.xaml.cs
+++ Search/SearchWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using CS3280GroupProject.Items;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,13 +21,32 @@ namespace CS3280GroupProject.Search
     /// </summary>
     public partial class SearchWindow : Window
     {
+        clsInvoice selectedInvoice;
+        clsInvoice filterInvoice;
+
         public SearchWindow()
         {
             InitializeComponent();
+
+            selectedInvoice = new clsInvoice();
+            filterInvoice = new clsInvoice();
+
+            // Populate the InvoiceNumber ComboBox with distinct invoice numbers
+            invoiceIDs.ItemsSource = clsSearchLogic.GetDistinctInvoiceNumbers();
+            // Populate the InvoiceDate ComboBox with distinct invoice dates
+            invoiceDates.ItemsSource = clsSearchLogic.GetDistinctDates();
+            // Populate the InvoiceCost ComboBox with distinct invoice costs
+            invoiceCosts.ItemsSource = clsSearchLogic.GetDistinctCosts();
+            // Populate the invoices DataGrid with all invoices
+            invoices.ItemsSource = clsSearchLogic.GetInvoices();
+
         }
 
-        // SelectedInvoiceID - Holds the invoice Id if the user selected one, and zero if no invoice selected
-
+        /// <summary>
+        /// Menu Item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="ev"></param>
         private void UpdateItems(object sender, EventArgs ev)
         {
             ItemWindow itemWnd = new ItemWindow();
@@ -34,9 +54,120 @@ namespace CS3280GroupProject.Search
             this.Close();
         }
 
-        private void mainMenu(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Menu Item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainMenu(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        /// <summary>
+        /// This event handles clearing the filters.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClearFilers_Click(object sender, RoutedEventArgs e)
+        {
+            invoiceIDs.SelectedIndex = -1; // Clear the selection in the ComboBox
+            invoiceDates.SelectedIndex = -1; // Clear the selected date in the ComboBox
+            invoiceCosts.SelectedIndex = -1; // Clear the selected cost in the ComboBox
+
+            selectedInvoice.Clear(); // Reset the selected invoice ID
+            filterInvoice.Clear(); // Reset the filter invoice
+
+            invoices.ItemsSource = clsSearchLogic.GetInvoices(); // Refresh the DataGrid with all invoices
+        }
+
+        /// <summary>
+        /// This event is triggered when the selection in the invoiceIDs ComboBox changes.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void invoiceIDs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (invoiceIDs.SelectedIndex == -1)
+                return;
+
+            filterInvoice.InvoiceID = (int)invoiceIDs.SelectedItem;
+
+            // If both InvoiceDate and TotalCost are set, filter the invoices
+            if (filterInvoice.InvoiceDate != DateTime.MinValue && filterInvoice.TotalCost != 0)
+            {
+                invoices.ItemsSource = clsSearchLogic.GetInvoices(filterInvoice.InvoiceID, filterInvoice.InvoiceDate, filterInvoice.TotalCost);
+                return;
+            }
+            else
+            {
+                if(filterInvoice.InvoiceDate != DateTime.MinValue)
+                {
+                    invoices.ItemsSource = clsSearchLogic.GetInvoices(filterInvoice.InvoiceID, filterInvoice.InvoiceDate);
+                    return;
+                }
+                else if (filterInvoice.TotalCost != 0)
+                {
+                    invoices.ItemsSource = clsSearchLogic.GetInvoices(filterInvoice.InvoiceID, filterInvoice.TotalCost);
+                    return;
+                }
+                else
+                {
+                    invoices.ItemsSource = clsSearchLogic.GetInvoices(filterInvoice.InvoiceID);
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This event is triggered when the selection in the invoiceDates ComboBox changes.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void invoiceDates_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// This event is triggered when the selection in the invoiceCosts ComboBox changes.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void invoiceCosts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// This handles the click event for the Select button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            
+            try 
+            {
+                clsInvoice selectedInvoice = invoices.SelectedItem as clsInvoice;
+                if (selectedInvoice != null)
+                {
+                    // Set the selected invoice ID
+                    this.selectedInvoice.InvoiceID = selectedInvoice.InvoiceID;
+                    this.selectedInvoice.InvoiceDate = selectedInvoice.InvoiceDate;
+                    this.selectedInvoice.TotalCost = selectedInvoice.TotalCost;
+                    // Close the search window
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Please select an invoice.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while selecting the invoice: {ex.Message}");
+            }
         }
     }
 }
--- Search/clsSearchLogic.cs
+++ Search/clsSearchLogic.cs
@@ -13,10 +13,119 @@ namespace CS3280GroupProject.Search
 {
     public class clsSearchLogic
     {
-        // GetDistinctInvoiceNumber
-        // GetDistinctDates
-        // GetDistinctCosts
-
         // GetInvoices(InvoiceNumber, InvoiceDate, InvoiceCost) - return List<clsInvoices>
+
+        public static List<int> GetDistinctInvoiceNumbers()
+        {
+            int iRetVal = 0;
+            List<int> invoiceNumbers = new List<int>();
+            DataSet ds = clsDataAccess.ExecuteSQLStatement(clsSearchSQL.GetDistinctInvoiceNumbers(), ref iRetVal);
+            if (iRetVal > 0)
+            {
+                invoiceNumbers = ds.Tables[0].AsEnumerable().Select(row => row.Field<int>("InvoiceNum")).ToList();
+            }
+            return invoiceNumbers;
+        }
+
+        public static List<DateTime> GetDistinctDates()
+        {
+            int iRetVal = 0;
+            List<DateTime> invoiceDates = new List<DateTime>();
+            DataSet ds = clsDataAccess.ExecuteSQLStatement(clsSearchSQL.GetDistinctDates(), ref iRetVal);
+            if (iRetVal > 0)
+            {
+                invoiceDates = ds.Tables[0].AsEnumerable().Select(row => row.Field<DateTime>("InvoiceDate")).ToList();
+            }
+            return invoiceDates;
+        }
+
+        public static List<decimal> GetDistinctCosts()
+        {
+            int iRetVal = 0;
+            List<decimal> invoiceCosts = new List<decimal>();
+            DataSet ds = clsDataAccess.ExecuteSQLStatement(clsSearchSQL.GetDistinctCosts(), ref iRetVal);
+            if (iRetVal > 0)
+            {
+                invoiceCosts = ds.Tables[0].AsEnumerable().Select(row => row.Field<decimal>("TotalCost")).ToList();
+            }
+            return invoiceCosts;
+        }
+
+
+
+
+
+        // GetInvoices methods start here
+
+        public static List<clsInvoice> GetInvoices()
+        {
+            int iRetVal = 0;
+            DataSet ds = clsDataAccess.ExecuteSQLStatement(clsSearchSQL.GetInvoices(), ref iRetVal);
+            
+            return GenerateData(ds, iRetVal);
+        }
+
+        public static List<clsInvoice> GetInvoices(int invoiceNum)
+        {
+            int iRetVal = 0;
+            DataSet ds = clsDataAccess.ExecuteSQLStatement(clsSearchSQL.GetInvoices(invoiceNum), ref iRetVal);
+
+            return GenerateData(ds, iRetVal);
+        }
+
+        public static List<clsInvoice> GetInvoices(int invoiceNum, DateTime invoiceDate)
+        {
+            int iRetVal = 0;
+            DataSet ds = clsDataAccess.ExecuteSQLStatement(clsSearchSQL.GetInvoices(invoiceNum, invoiceDate), ref iRetVal);
+
+            return GenerateData(ds, iRetVal);
+        }
+
+        public static List<clsInvoice> GetInvoices(int invoiceNumber, DateTime invoiceDate, decimal invoiceCost)
+        {
+            int iRetVal = 0;
+            DataSet ds = clsDataAccess.ExecuteSQLStatement(clsSearchSQL.GetInvoices(invoiceNumber, invoiceDate, invoiceCost), ref iRetVal);
+            
+            return GenerateData(ds, iRetVal);
+        }
+
+        public static List<clsInvoice> GetInvoices(decimal totalCost)
+        {
+            int iRetVal = 0;
+            DataSet ds = clsDataAccess.ExecuteSQLStatement(clsSearchSQL.GetInvoices(totalCost), ref iRetVal);
+
+            return GenerateData(ds, iRetVal);
+        }
+
+        public static List<clsInvoice> GetInvoices(int invoiceNum, decimal totalCost)
+        {
+            int iRetVal = 0;
+            DataSet ds = clsDataAccess.ExecuteSQLStatement(clsSearchSQL.GetInvoices(invoiceNum, totalCost), ref iRetVal);
+
+            return GenerateData(ds, iRetVal);
+        }
+
+        public static List<clsInvoice> GetInvoices(DateTime invoiceDate)
+        {
+            int iRetVal = 0;
+            DataSet ds = clsDataAccess.ExecuteSQLStatement(clsSearchSQL.GetInvoices(invoiceDate), ref iRetVal);
+
+            return GenerateData(ds, iRetVal);
+        }
+
+        private static List<clsInvoice> GenerateData(DataSet ds, int iRetVal)
+        {
+            List<clsInvoice> invoices = new List<clsInvoice>();
+            if (iRetVal > 0)
+            {
+                invoices = ds.Tables[0].AsEnumerable().Select(row => new clsInvoice
+                {
+                    InvoiceID = row.Field<int>("InvoiceNum"),
+                    InvoiceDate = row.Field<DateTime>("InvoiceDate"),
+                    TotalCost = row.Field<decimal>("TotalCost")
+                }).ToList();
+            }
+            return invoices;
+        }
     }
 }
