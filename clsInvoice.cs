e8d395aaffaa4587031eb67fa4096308cae9de99
Jake Runyan <jake.runyan5@gmail.com>
2025-08-06 22:19:49 -0600

    Created an invoice object

---
+++ clsInvoice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3280GroupProject
+{
+    public class clsInvoice
+    {
+        public int InvoiceID { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public decimal TotalCost { get; set; }
+
+        public void Clear()
+        {
+            this.InvoiceID = 0;
+            this.InvoiceDate = DateTime.MinValue;
+            this.TotalCost = 0.0m;
+        }
+    }
+}
